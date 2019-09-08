f782a10eae18a028a7d5ef0eeafc3b7c0c7beb38
George <george995@gmail.com>
2019-09-08 21:23:53 +1000

    George: Test Classes added

---
+++ src/pacman/Test/TestGameLogic.java
@@ -0,0 +1,14 @@
+package pacman.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGameLogic {
+	
+	@Test
+	public void testIsValidPlayerPossition() {
+		fail("Not yet implemented");
+	}
+
+}
